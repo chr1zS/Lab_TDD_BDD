96f00703613709e96b165b746cc958c02d6ea837
Christian Suarez <Chris@Mac.local>
2021-05-26 00:36:56 +0200

    creación de prueba para la función suma

---
+++ src/calculadora.js
@@ -0,0 +1,18 @@
+const suma = (a,b) => {
+	
+}
+
+//Prueba de la función suma
+consr prueba_suma = () => {
+	//Probar que 2 +5 devuelve 7
+	//arrange
+	const a = 2;
+	const b = 5;
+	const esperado = 7;
+	//act
+	const resultado = suma(a,b);
+	//assert
+	console.assert(resultado===esperado, "fallo en la función suma");
+}
+
+prueba_suma()
