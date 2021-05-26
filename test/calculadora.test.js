47990be9178e1a69b6cf2909b83b960d259f61cf
Christian Suarez <Chris@Mac.local>
2021-05-26 12:23:49 +0200

    añado codigo modules/calduladora.js

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,4 +1,6 @@
-const { test, expect } = require("@jest/globals");
+const { test } = require("@jest/globals");
+const suma = require("../modules/calculadora");
+
 
 test("La suma de 2 y 5 devuelve 7", () => {
     //arrange
@@ -6,8 +8,8 @@ test("La suma de 2 y 5 devuelve 7", () => {
     const b = 5;
     const esperado = 7;
     //act
-    const resultado = suma(a, b);
+    const resultado = suma(a,b);
     //assert
-    expect(resultado).toBe(esperado)
+    expect(resultado).toBe(esperado);
 
 })
