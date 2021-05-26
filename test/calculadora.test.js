7ffac758021a03846692407f86a262ef2afdeb9d
Christian Suarez <Chris@Mac.local>
2021-05-26 22:58:16 +0200

    cambio nombre a modulo calculadora.js por suma.js

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,5 +1,5 @@
 const { test } = require("@jest/globals");
-const suma = require("../modules/calculadora");
+const suma = require("../modules/suma");
 
 
 test("La suma de 2 y 5 devuelve 7", () => {
@@ -19,18 +19,6 @@ test("La suma de -2 y 4 devuelve 2", () => {
     const a = -2;
     const b = 4;
     const esperado = 2;
-    //act
-    const resultado = suma(a,b);
-    //assert
-    expect(resultado).toBe(esperado);
-
-})
-
-test("La suma de 1.4 y 4.3 devuelve 2", () => {
-    //arrange
-    const a = 1.4;
-    const b = 4.3;
-    const esperado = 5.7;
     //act
     const resultado = suma(a,b);
     //assert
