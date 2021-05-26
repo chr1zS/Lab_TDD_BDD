b7ca32288d9dbf8bd4222a275f534919e13a0671
Christian Suarez <Chris@Mac.local>
2021-05-26 22:50:59 +0200

    Añado otro test para la suma

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -13,3 +13,28 @@ test("La suma de 2 y 5 devuelve 7", () => {
     expect(resultado).toBe(esperado);
 
 })
+
+test("La suma de -2 y 4 devuelve 2", () => {
+    //arrange
+    const a = -2;
+    const b = 4;
+    const esperado = 2;
+    //act
+    const resultado = suma(a,b);
+    //assert
+    expect(resultado).toBe(esperado);
+
+})
+
+test("La suma de 1.4 y 4.3 devuelve 2", () => {
+    //arrange
+    const a = 1.4;
+    const b = 4.3;
+    const esperado = 5.7;
+    //act
+    const resultado = suma(a,b);
+    //assert
+    expect(resultado).toBe(esperado);
+
+})
+
