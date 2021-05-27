46ca10a31dd212b2194cbcd7989593ad8a668570
Christian Suárez <cesh_christian@yahoo.es>
2021-05-28 00:28:17 +0200

    Añado tolerancia y error cometido en el test de la raíz y corrijo test multiplicación (faltaba el act)

--- test/multiplicacion.test.js
+++ test/multiplicacion.test.js
@@ -1,4 +1,5 @@
 const { test, expect } = require("@jest/globals");
+const multiplicacion = require("../modules/multiplicacion");
 
 
 
@@ -8,7 +9,7 @@ test("La multiplicación de 11 y 3 devuelve 33", () => {
     const b = 3;
     const esperado = 33;
     //act
-    const resultado =
+    const resultado = multiplicacion(a, b);
     //assert
     expect(resultado).toBe(esperado);
 
@@ -20,7 +21,7 @@ test("La multiplicación de 1548 y 0 devuelve 0", () => {
     const b = 0;
     const esperado = 0;
     //act
-    const resultado = 
+    const resultado = multiplicacion(a, b);
     //assert
     expect(resultado).toBe(esperado);
 
--- test/raiz.test.js
+++ test/raiz.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require("@jest/globals");
 const { ERROR } = require("jest-validate/build/utils");
 const raiz_cuad = require("../modules/raiz");
+const resta = require("../modules/resta");
 
 
 
@@ -11,7 +12,10 @@ test("La raíz de 49 devuelve 7", () => {
     //act
     const resultado = raiz_cuad(a);
     //assert
+    const tolerancia = 0.001;
+    const error_cometido = resta (resultado, esperado);
     expect(resultado).toBe(esperado);
+    expect(error_cometido).toBeLessThan(tolerancia);
 
 })
 
@@ -22,29 +26,38 @@ test("La raíz de -85 devuelve 0", () => {
     //act
     const resultado = raiz_cuad(a);
     //assert
+    const tolerancia = 0.001;
+    const error_cometido = resta (resultado, esperado);
     expect(resultado).toBe(esperado);
+    expect(error_cometido).toBeLessThan(tolerancia);
 
 })
 
-est("La raíz de 24 devuelve 4.898979485", () => {
+test("La raíz de 24 devuelve 4.898979485", () => {
     //arrange
     const a = 24;
     const esperado = 4.898979485;
     //act
     const resultado = raiz_cuad(a);
     //assert
+    const tolerancia = 0.001;
+    const error_cometido = resta (resultado, esperado);
     expect(resultado).toBe(esperado);
+    expect(error_cometido).toBeLessThan(tolerancia);
 
 })
 
-est("La raíz de 0 devuelve 0", () => {
+test("La raíz de 0 devuelve 0", () => {
     //arrange
     const a = 0;
     const esperado = 0;
     //act
     const resultado = raiz_cuad(a);
     //assert
+    const tolerancia = 0.001;
+    const error_cometido = resta (resultado, esperado);
     expect(resultado).toBe(esperado);
+    expect(error_cometido).toBeLessThan(tolerancia);
 
 })
 
